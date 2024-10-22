f1fb394f13b95763832363e55260f891319e89a7
VSNSRUJANA123 <srujanadvsn@gmail.com>
2024-10-22 18:26:32 +0530

    frontend

---
+++ src/MainContent.js
@@ -0,0 +1,20 @@
+import "./App.css";
+const MainContent = () => {
+  return (
+    <div className="home-banner">
+      <div className="text-background">
+        <h1 className="text-white">NEED A Mental Health PROVIDER? </h1>
+        <h6 className="text-white">No Look Further</h6>
+        <p className="text-white">
+          Our goal is to provide a safe, comfortable, and warm environment so
+          that you can openly discuss your mental health needs.
+        </p>
+        <button className="button text-black rounded-2xl text-xs p-2 pr-3 pl-3">
+          Learn More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
